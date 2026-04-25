3febb47ea437a69f81267ce8caf906cb4f5a1c74
Mark Balajadia <markb.freelance@gmail.com>
2026-04-25 14:14:13 +0800

    next config update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ["http://127.0.0.1:*", "127.0.0.1"],
+  images: {
+    remotePatterns: [
+      { hostname: "picsum.photos" },
+      { hostname: "images.unsplash.com" },
+    ],
+  },
 };
 
 export default nextConfig;
